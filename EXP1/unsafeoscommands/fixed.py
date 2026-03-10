e7cd90435d346a88b558ab08349bea446f70be02
Anshuman Raj Singh <anshumanrajsingh16@gmail.com>
2026-03-10 08:12:38 +0530

    fixed.py

--- EXP1/unsafeoscommands/fixed.py
+++ EXP1/unsafeoscommands/fixed.py
@@ -3,9 +3,4 @@ user = input("Enter your name: ")
 command = "echo Hello " + user
 os.system(command)
 
-# use input as : "filename" ; ls 
-            # or   "filename" ; whoami
-            # or   "filename" ; uname -a
-            # or   "filename" ; pwd 
-
-
+# use input as : "filename" 
